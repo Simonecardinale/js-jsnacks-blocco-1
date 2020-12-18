d61754e015c9a64b34a62b14fc0eebf16c280fc4
Simone Cardinale <simonecardinale8@gmail.com>
2020-12-18 18:52:24 +0100

    Work complete + bonus on snack 4 and 5
    creation of a cicle to reach the target

--- snack 5/script.js
+++ snack 5/script.js
@@ -1,17 +1,49 @@
 // L’utente inserisce due parole in successione, con due prompt. Il software stampa prima la parola più corta, poi la parola più lunga.
 
 
-var utente1 = prompt("Inserisci una parola");
-var utente2 = prompt("Inserisci una parola");
+// var utente1 = prompt("Inserisci una parola");
+// var utente2 = prompt("Inserisci una parola");
 
-if (utente1.length < utente2.length) {
-    console.log(utente1);
-} else {
-    console.log(utente2);
+// if (utente1.length < utente2.length) {
+//     console.log(utente1);
+// } else {
+//     console.log(utente2);
+// };
+
+// if (utente1.length > utente2.length) {
+//     console.log(utente1);
+// } else {
+//     console.log(utente2);
+// };
+
+
+var array =[];
+
+for (var i = 0; i < 2; i++) {
+    var utente = prompt("Inserisci una parola");
+    array.push(utente);
+}
+
+// console.log(array[0].length);
+
+var max = "";
+// console.log(minimo);
+
+for(var i = 0; i < array.length; i++) {
+    parolaLetta = array[i];
+    // console.log(parolaLetta);
+    if (parolaLetta.length > max.length) {
+        max = parolaLetta;
+    }
+};
+console.log(max);
+
+for(var i = 0; i < array.length; i++) {
+    parolaLettaPiccola = array[i];
+    // console.log(parolaLetta);
+    if (parolaLettaPiccola.length < parolaLetta.length) {
+        parolaLetta = parolaLettaPiccola;
+    }
 };
 
-if (utente1.length > utente2.length) {
-    console.log(utente1);
-} else {
-    console.log(utente2);
-};
+console.log(parolaLetta);
